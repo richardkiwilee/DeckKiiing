c2add07bd779a9f2acd2a779492681dc4feda4b5
Li Ruicun <lirc@jindefund.com>
2020-07-28 10:16:21 +0800

    init

--- setup.py
+++ setup.py
@@ -1,37 +1,32 @@
 from setuptools import setup, find_packages
 
-
-def version():
-    with open(r'.\deckiiing\__version__.py', 'r') as v:
-        content = v.read().split('\n')[0]
-    return content.split('=')[-1]
+with open('requirements.txt', 'r') as f:
+    requires = [line.rstrip() for line in f.readlines()]
 
 
-def requirements():
-    with open('requirements.txt', 'r') as r:
-        content = r.readlines()
-    res = []
-    for item in content:
-        res.append( item.split('\n')[0])
-    return res
+def get_version():
+    u""" 从__version__.py中读取版本号。 """
+    with open('deckiiing/__version__.py', encoding='utf-8-sig') as version_file:
+        for line in version_file:
+            if line.startswith('__version__'):
+                return eval(line.split('=')[-1])
 
 
 if __name__ == '__main__':
-
     setup(
-        name="deckiiing",
-        version=version(),
-        description="Deck Building Master System",
-        license="MIT Licence",
-        author="yujj&lirc",
+        name='deckiiing',
+        description='Deck Building Master System',
+        version=get_version(),
         packages=find_packages(),
+        entry_points={'console_scripts': ['deckiiing=deckiiing:cli']},
+        zip_safe=False,
+        license="MIT Licence",
+        package_data={
+            'deckiiing': ['bin/*.*']
+        },
         include_package_data=True,
-        platforms="any",
-        install_requires=requirements(),
-        scripts=[],
-        entry_points={
-            'console_scripts': [
-                'deckiiing = deckiiing:main'
-            ]
-        }
+        install_requires=requires,
+        author='yujj&lirc',
+        python_requires='>=3.6, <3.8',
+        platforms="win_amd64"
     )
